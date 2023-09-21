7c30b7ac9b81b0aee0ed827737ed3c058e6ea7d2
WaterbendingToph <dakotago@lakotaarms45@gmail.com>
2023-09-21 16:02:41 -0600

    Setup Essence Experimenter Page Form Not Function

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,23 +2,28 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
-const STYLES = ['butn--primary', 'btn--outline']
+const STYLES = ['btn--primary', 'btn--outline']
 
 const SIZES = ['btn--medium', 'btn--large'];
+
+const TARGETS = ['/'];
 
 export const Button = ({
     children,
     type, 
     onClick, 
     buttonStyle, 
-    buttonSize
+    buttonSize,
+    target
 }) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
 
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
+    const checkButtonTarget = TARGETS.includes(target) ? target : TARGETS[0];
+
     return (
-        <Link to='/sign-up' className='btn-mobile'>
+        <Link to={target} className='btn-mobile'>
             <button 
             className={`btn ${checkButtonStyle} ${checkButtonSize}` } 
             onClick={onClick}
--- src/components/pages/EssenceExperimenter.js
+++ src/components/pages/EssenceExperimenter.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import '../../App.css';
 import './pages.css';
+import './EssenceExperimenter.css';
+import { Button } from '../Button';
+import '../Button.css';
 
 function EssenceExperimenter () {
     return (
-        <div className='essenceExperimenter-container'>
+        <div id='overall-container'>
             <h1>ESSENCE EXPERIMENTER PAGE</h1>
+            <div className='row-1'>
+                <div className='base-essence-container'>
+                    <div className='essence-square' id='base-essence-1' />
+                    <h2 id='essence-name-1'>Essence Name 1</h2>
+                </div>
+                
+                <div className='base-essence-container'>
+                    <div className='essence-square' id='base-essence-2'/>
+                    <h2 id='essence-name-2'>Essence Name 2</h2>
+                </div>
+                
+                <div className='base-essence-container'>
+                    <div className='essence-square' id='base-essence-3'/>
+                    <h2 id='essence-name-3'>Essence Name 3</h2>
+                </div>
+            </div>
+
+            <div className='row-2'>
+                <div className='button-container'>
+                    <Button target={'/WorldDetails'}>Essence 1 Details</Button>
+                    <Button target={'/WorldDetails'}>Essence 2 Details</Button>
+                    <Button target={'/WorldDetails'}>Essence 3 Details</Button>
+                </div>
+                
+                <div id='confluence-essence-container' >
+                    <div className='confluence-essence-square' ></div>
+                    <h2 id='confluence-essence-name'>Confluence Essence Name</h2> 
+                </div>
+
+                <div className='button-container'>
+                    <Button target={'/WorldDetails'}>Randomize</Button> 
+                    <Button target={'/WorldDetails'}>Confluence Essence Details</Button>
+                </div>
+            </div>
         </div>
     )
 }
